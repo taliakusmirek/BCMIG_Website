7175336aa403f7b7d938ba56e802502b6556a1b9
Natalia Kusmirek <nekusmirek@wpi.edu>
2024-10-08 15:50:01 -0400

    lets try again!

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
